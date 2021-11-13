4d1167e6cdd617d8acf8707ae65ebf118b487607
arozqq <ajibroziq21@gmail.com>
2021-11-13 16:01:16 +0700

    create vue frontend, tampil data

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+//import router
+import router from "./Router";
+
+// import bootstrao, popper, jquery
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.js';
+
+const app = createApp(App);
+// use vue Router
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/post/Index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>Data Post</h4>
+                        <hr>
+                        <router-link :to="{name: 'post.create'}" class="btn btn-primary">Tambah Data</router-link>
+                        
+                        <table class="table table-striped table-bordered mt-5">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Content</th>
+                                    <th scope="col">Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(post, index) in posts" :key="index">
+                                    <td>{{post.title}}</td>
+                                    <td>{{post.content}}</td>
+                                    <td class="text-center">
+                                        <router-link :to="{name: 'post.edit', params:{id: post.id}}" class="btn btn-sm btn-warning">Edit</router-link>
+                                        <button class="btn btn-danger btn-sm mx-2">Delete</button>
+                                    </td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//untuk ambil data dari api laravel8
+import axios from 'axios'
+import { onMounted, ref} from 'vue'
+
+    export default {
+        setup() {
+            
+            // reactive state
+            let posts = ref([])
+            
+            //mounted / memasang
+            onMounted(() => {
+                // ambil api dari laravel backend api
+                axios.get('http://lara8_api.test/api/post')
+                .then(response => {
+
+                    // masukan data dan respose data
+                    posts.value = response.data.data
+                }).catch(error => {
+                    console.log(error.response.data)
+                })
+            })
+
+         return {
+            posts
+         }      
+
+        }
+
+     }
+</script>
